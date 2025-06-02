03fc3b89d4ed543a819414da8659fdb2090466eb
lemiil <gavriillemeshko10@gmail.com>
2025-06-02 16:56:52 +0300

    changed front

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+	},
     plugins: [
+		tailwindcss(),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
