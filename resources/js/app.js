24d748155a9b2ea13985508c46998d4a2c139f3b
lemiil <gavriillemeshko10@gmail.com>
2025-06-02 17:20:47 +0300

    fix: front doesnt loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import '../css/app.css';
+
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	server: {
-		host: '0.0.0.0',
-		port: 5173,
-	},
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            port: 5173,
+            host: 'localhost'
+        }
+    },
     plugins: [
-		tailwindcss(),
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
         vue({
