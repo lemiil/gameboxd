3f460e6c671ef69d6d919e7f7d0bda2ec4f94548
lemiil <gavriillemeshko10@gmail.com>
2025-06-04 11:41:44 +0300

    update: add some db

---
+++ database/migrations/2025_06_04_082357_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('release_date')->nullable();
+            $table->string('cover_url')->nullable();
+            $table->string('artwork_url')->nullable();
+            $table->text('summary')->nullable();
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
