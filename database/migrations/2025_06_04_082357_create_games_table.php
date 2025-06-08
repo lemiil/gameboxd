a1d1671d9e6d867d04ec90f1fb0be7867c2f774f
lemiil <gavriillemeshko10@gmail.com>
2025-06-08 13:41:09 +0300

    delete: artwork

--- database/migrations/2025_06_04_082357_create_games_table.php
+++ database/migrations/2025_06_04_082357_create_games_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->date('release_date')->nullable();
             $table->string('cover_url')->nullable();
-            $table->string('artwork_url')->nullable();
             $table->text('summary')->nullable();
             $table->timestamps();
         });
