e5f3b711fa41842ddba49b95c054e2d12729f698
lemiil <gavriillemeshko10@gmail.com>
2025-06-16 10:50:58 +0300

    update: add reviews

---
+++ app/Models/Review.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Review extends Model
+{
+    protected $fillable = ['rating', 'status', 'user_id', 'game_id', 'platform_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function game(): BelongsTo
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function platform(): BelongsTo
+    {
+        return $this->belongsTo(Platform::class);
+    }
+}
